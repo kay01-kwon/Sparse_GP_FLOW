b986e98f63f58b9859a93e3251be620af895eaae
geonwoo kwon <46738866+kay01-kwon@users.noreply.github.com>
2024-03-29 23:16:32 +0900

    Add files via upload

--- server.py
+++ server.py
@@ -18,6 +18,10 @@ class SGPserver:
         self.saved_dir = saved_dir
         print(self.saved_dir)
 
+
+        # GPU memory limit setup
+        self.gpu_setup()
+
         # Load the saved model
         self.loaded_model = self.load_saved_model()
         # Sets PORT
@@ -25,7 +29,25 @@ class SGPserver:
 
         self.conn, self.addr = self.socket_handler()
 
+        self.tau_motor = np.zeros((3,1))
+        self.omega = np.zeros((3,1))
+        self.tau_fric = np.zeros((3,1))
+        self.tau_fric_var = np.zeros((3,1))
 
+        self.X_vstack = np.zeros((2,3))
+
+    def gpu_setup(self):
+        gpus = tf.config.list_physical_devices('GPU')
+        if gpus:
+            # Restrict tf to only allocate 1 GB of memory on the first GPU
+            try:
+                tf.config.set_logical_device_configuration(
+                    gpus[0],
+                    [tf.config.LogicalDeviceConfiguration(memory_limit = 1024)])
+                logical_gpus = tf.config.list_logical_devices('GPU')
+                print(len(gpus), "Physical GPUs", len(logical_gpus), "Logical GPUs")
+            except RuntimeError as e:
+                print(e)
 
     def load_saved_model(self):
         saved_model = tf.saved_model.load(self.saved_dir)
@@ -52,48 +74,65 @@ class SGPserver:
 
     def SGPR_run(self):
         while True:
-            input_data = self.conn.recv(16)
+            input_data = self.conn.recv(100)
             if input_data:
-                tau_meas, omega_meas = self.get_input(input_data)
-                tau_fric_mean, tau_fric_var = self.get_tau_fric(tau_meas, omega_meas)
-                self.send_tau_fric(tau_fric_mean, tau_fric_var)
+                self.get_input(input_data)
+                self.get_tau_fric()
+                self.send_tau_fric()
         self.conn.close()
 
 
     def get_input(self, input_data):
         """
         Gets input data from the client and convert it to numpy float64 format.
-        :param input_data: byte array of tau_meas and omega_meas
-        :return: tau_meas, omega_meas
+        :param input_data: byte array of tau_motor and omega
+        :return: tau_motor, omega
         """
-        tau_meas = np.float64(struct.unpack('d', input_data[0:8]))
-        oemga_meas = np.float64(struct.unpack('d', input_data[8:]))
-        print("tau_meas: ", tau_meas, "oemga_meas: ",oemga_meas)
-        return tau_meas, oemga_meas
+        for i in range(3):
+            self.tau_motor[i] = np.float64(struct.unpack('d', input_data[8*i:8*(i+1)]))
+        
+        offset = 24
+        for i in range(3):
+            self.omega[i] = np.float64(struct.unpack('d', input_data[8*i + offset:8*(i+1)+offset]))
+        
 
-    def get_tau_fric(self, tau_meas, oemga_meas):
+    def get_tau_fric(self):
         """
         From the saved SGPR model, get the mean and variance of the tau_fric, respectively.
-        :param tau_meas:
-        :param oemga_meas:
+        :param tau_motor:
+        :param oemga:
         :return: tau_fric_mean, tau_fric_var
         """
-        X_vstack = np.vstack((tau_meas, oemga_meas)).T
-        tau_fric_mean, tau_fric_var = self.loaded_model.compiled_predict_f(X_vstack)
-        return tau_fric_mean, tau_fric_var
 
-    def send_tau_fric(self, tau_fric_mean, tau_fric_var):
+        X_vstack = np.vstack((self.tau_motor.flatten(), self.omega.flatten())).T
+
+        print('-'*30)
+        print('Input matrix')
+        print(X_vstack)
+        print('-'*30)
+        for i in range(3):
+            self.tau_fric[i], self.tau_fric_var[i] = \
+            self.loaded_model.compiled_predict_f(X_vstack[i,:].reshape(1,2))
+
+    def send_tau_fric(self):
         """
         Converts into byte array and sends it to the client
         :param tau_fric_mean:
         :param tau_fric_var:
         :return:
         """
-        tau_fric_mean_ = tau_fric_mean.numpy()
-        tau_fric_var_ = tau_fric_var.numpy()
-        print("tau_fric_mean: ",tau_fric_mean_, "tau_fric_var:", tau_fric_var_)
-        bytearray_ = bytearray(struct.pack('d',tau_fric_mean_))
-        self.conn.sendall(bytearray_)
+
+        print('-'*30)
+        print('Friction')
+        print(self.tau_fric)
+        print('-'*30)
+
+        for i in range(3):
+            if i == 0:
+                byte_array = bytearray(struct.pack('d',self.tau_fric[i]))
+                continue
+            byte_array =  byte_array + bytearray(struct.pack('d',self.tau_fric[i]))
+        self.conn.sendall(byte_array)
 
 
 if __name__ == "__main__":
@@ -101,34 +140,3 @@ if __name__ == "__main__":
     saved_dir = current_dir + "/saved_model"
     SGP_server = SGPserver(saved_dir)
     SGP_server.SGPR_run()
-
-
-# PORT = 1029
-#
-# data = []
-# data = np.float64(5.1)
-# byte_array = bytearray(struct.pack("d", data))
-#
-# save_dir = "saved_model"
-# loaded_model = tf.saved_model.load(save_dir)
-#
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#     s.bind(('localhost', PORT))
-#     s.listen()
-#     conn, addr = s.accept()
-#     with conn:
-#         print('Connected by', addr)
-#         while True:
-#             data_recv = conn.recv(16)
-#             if data_recv:
-#                 data_recv1 = np.float64(struct.unpack("d", data_recv[0:8]))
-#                 data_recv2 = np.float64(struct.unpack("d", data_recv[8:]))
-#                 X_vstack = np.vstack((data_recv1, data_recv2)).T
-#                 f_mean, f_var = loaded_model.compiled_predict_f(X_vstack)
-#                 data_send = f_mean.numpy()
-#                 print('data1', data_recv1, '+ data2', data_recv2, '= result',data_send)
-#                 byte_array = bytearray(struct.pack("d", data_send))
-#                 conn.sendall(byte_array)
-#             # time.sleep(1)
-#     print("Connection closed")
-#     conn.close()
